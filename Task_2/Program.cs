e3232f49e6c8d4a87ad60cfba2233f2dcba5651d
Julia <yulya_si@mail.ru>
2023-07-15 11:59:42 +0300

    Add Task

---
+++ Task_2/Program.cs
@@ -0,0 +1,62 @@
+﻿// Задача 2. Напишите программу, которая на вход принимает позиции элемента в двумерном массиве, и возвращает значение этого элемента или же указание, что такого элемента нет.
+
+// Например, задан массив:
+// 1 4 7 2
+// 5 9 2 3
+// 8 4 2 4
+// 17 -> такого числа в массиве нет
+
+int[,] CreateArr(int rows, int cols) 
+{
+    int[,] arrayD2 = new int[rows, cols];
+
+    for (int i = 0; i < arrayD2.GetLength(0); i++)
+    {
+        for (int j = 0; j < arrayD2.GetLength(1); j++)
+        {
+            arrayD2[i, j] = new Random().Next(0, 10);
+        }
+    }
+    return arrayD2;
+}
+
+void ShowArray(int[,] array) 
+{
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            System.Console.Write($"{array[i, j]}\t");
+        }
+        System.Console.WriteLine();
+    }
+}
+
+int ReadInt(string message)
+{
+    System.Console.Write($"{message} > ");
+    string inputedString = Console.ReadLine();
+    if (int.TryParse(inputedString, out int convertedInt))
+    {
+        return convertedInt;
+    }
+    System.Console.WriteLine("Вы ввели не число");
+    Environment.Exit(0);
+    return 0;
+}
+
+int rows = ReadInt("Введите число строк");
+int columns = ReadInt("Введите число столбцов");
+int[,] arr = CreateArr(rows, columns);
+ShowArray(arr);
+int n = ReadInt("Введите номер строки");
+int m = ReadInt("Введите номер столбца");
+
+if (n > rows || m > columns)
+{
+    System.Console.WriteLine("Такого элемента нет");
+}
+else
+{
+    System.Console.WriteLine($"Значение элемента {n} строки и {m} столбца равно {arr[n-1, m-1]}");
+}
---
+++ Task_3/Program.cs
@@ -0,0 +1,63 @@
+﻿// Задача 3. Задайте двумерный массив из целых чисел. Найдите среднее арифметическое элементов в каждом столбце.
+
+// Например, задан массив:
+// 1 4 7 2
+// 5 9 2 3
+// 8 4 2 4
+// Среднее арифметическое каждого столбца: 4,6; 5,6; 3,6; 3.
+
+int[,] CreateArr(int rows, int cols) 
+{
+    int[,] arrayD2 = new int[rows, cols];
+
+    for (int i = 0; i < arrayD2.GetLength(0); i++)
+    {
+        for (int j = 0; j < arrayD2.GetLength(1); j++)
+        {
+            arrayD2[i, j] = new Random().Next(0, 10);
+        }
+    }
+    return arrayD2;
+}
+
+void ShowArray(int[,] array) 
+{
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            System.Console.Write($"{array[i, j]}\t");
+        }
+        System.Console.WriteLine();
+    }
+}
+
+int ReadInt(string message)
+{
+    System.Console.Write($"{message} > ");
+    string inputedString = Console.ReadLine();
+    if (int.TryParse(inputedString, out int convertedInt))
+    {
+        return convertedInt;
+    }
+    System.Console.WriteLine("Вы ввели не число");
+    Environment.Exit(0);
+    return 0;
+}
+
+int rows = ReadInt("Введите число строк");
+int columns = ReadInt("Введите число столбцов");
+int[,] arr = CreateArr(rows, columns);
+ShowArray(arr);
+System.Console.WriteLine();
+System.Console.Write($"Cреднее арифметическое элементов каждого столбца равно: ");
+for (int j = 0; j < arr.GetLength(1); j++)
+{
+    double avarage = 0;
+    for (int i = 0; i < arr.GetLength(0); i++)
+    {
+        avarage = (avarage + arr[i, j]);
+    }
+    avarage = avarage / rows;
+    System.Console.Write($"{avarage:F1}; ");
+}
